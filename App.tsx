dd6c3f05732e96962536eb21d24cd9bface65fd9
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-11-28 19:43:44 -0500

    Update code to accept data array

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
-import {View, StyleSheet} from 'react-native';
 import React from 'react';
-
+import {View, StyleSheet} from 'react-native';
 import Slider from './src/components/Slider';
 
 const App = () => {
+  const sliderData = [
+    {
+      text: 'Press',
+      iconName: 'accessibility-outline',
+      extraContent: 'below to start your journey to a less stressful life.',
+    },
+    {
+      text: 'Check out',
+      iconName: 'settings-outline',
+      extraContent: 'below to configure your meditation experience.',
+    },
+    {
+      text: 'Use',
+      iconName: 'help-circle-outline',
+      extraContent:
+        'on the top right from any screen to send us your feedback.',
+    },
+    {
+      text: 'Tap',
+      iconName: 'share-outline',
+      extraContent:
+        'on Exercises screen to share your Resilient Moments with others.',
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Slider />
+      <Slider data={sliderData} />
     </View>
   );
 };
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
-import {View, StyleSheet, Text} from 'react-native';
 import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
 import Swiper from 'react-native-swiper';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {moderateScale} from 'react-native-size-matters';
@@ -36,47 +36,24 @@ const customActiveDot = (
   />
 );
 
-const Slider = () => {
+const Slider = ({data}: any) => {
   return (
     <Swiper
       style={styles.wrapper}
       showsButtons={true}
-      showsPagination={true} // Show pagination dots
-      paginationStyle={styles.pagination} // Customize pagination container style
-      dot={customDot} // Customize inactive dot
-      activeDot={customActiveDot} // Customize active dot
-    >
-      <View style={styles.slide1}>
-        <Text style={styles.text}>
-          Press{' '}
-          <Ionicons
-            name="accessibility-outline"
-            size={iconSize}
-            color="white"
-          />{' '}
-          below to start your journey to a less stressful life.
-        </Text>
-      </View>
-      <View style={styles.slide2}>
-        <Text style={styles.text}>
-          Check out{' '}
-          <Ionicons name="settings-outline" size={iconSize} color="white" />{' '}
-          below to configure your meditation experience.
-        </Text>
-      </View>
-      <View style={styles.slide3}>
-        <Text style={styles.text}>
-          Use{' '}
-          <Ionicons name="help-circle-outline" size={iconSize} color="white" />{' '}
-          on the top right from any screen to send us your feedback.
-        </Text>
-      </View>
-      <View style={styles.slide4}>
-        <Text style={styles.text}>
-          Tap <Ionicons name="share-outline" size={iconSize} color="white" /> on
-          Exercises screen to share your Resilient Moments with others.
-        </Text>
-      </View>
+      showsPagination={true}
+      paginationStyle={styles.pagination}
+      dot={customDot}
+      activeDot={customActiveDot}>
+      {data.map((item: any, index: any) => (
+        <View key={index} style={styles.slide}>
+          <Text style={styles.text}>
+            {item.text}{' '}
+            <Ionicons name={item.iconName} size={iconSize} color="white" />{' '}
+            {item.extraContent}
+          </Text>
+        </View>
+      ))}
     </Swiper>
   );
 };
@@ -84,43 +61,16 @@ const Slider = () => {
 const styles = StyleSheet.create({
   wrapper: {},
   pagination: {
-    bottom: 250, // Adjust the bottom spacing for pagination
+    bottom: 250,
   },
-  slide1: {
+  slide: {
     flex: 0.3,
-    marginTop: moderateScale(260),
-    margin: moderateScale(23),
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
     borderRadius: 20,
-  },
-  slide2: {
-    flex: 0.3,
-    marginTop: moderateScale(260),
     margin: moderateScale(23),
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    borderRadius: 20,
-  },
-  slide3: {
-    flex: 0.3,
     marginTop: moderateScale(260),
-    margin: moderateScale(23),
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    borderRadius: 20,
-  },
-  slide4: {
-    flex: 0.3,
-    marginTop: moderateScale(260),
-    margin: moderateScale(23),
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    borderRadius: 20,
   },
   text: {
     margin: 5,
